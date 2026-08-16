cb607f88de07feab2b3aefccf82d96144bc8480d
Tulsi Halwai <tulsihalwai@gmail.com>
2026-08-16 14:58:40 +0530

    Initial commit

---
+++ App_Data/DataAcess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _3T_Arcitecture
+{
+    public class DataAcess
+    {
+    }
+}
---
+++ obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3T_Arcitecture.Global" Language="C#" %>
